dca85b496d939fee4bbd74e18075933dc68420e5
Oscar-Samsung <orgimeno@gmail.com>
2025-12-16 14:02:58 +0100

    backend: CORS conf

---
+++ backend/src/config/cors.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    // Solo API (evita tocar rutas web)
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    // Permitimos todos los métodos necesarios
+    'allowed_methods' => ['*'],
+
+    // Permitimos el origen del frontend (Vite)
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    // Headers típicos
+    'allowed_headers' => ['*'],
+
+    // No usamos cookies/auth, así que false
+    'supports_credentials' => false,
+
+    // Opcional
+    'exposed_headers' => [],
+    'max_age' => 0,
+];
